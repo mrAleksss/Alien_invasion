fc496dd4a2fc1190adbfe59ff584b3802b3daec7
mr_X <mr.Aleksandrsmith@gmail.com>
2023-03-29 15:10:40 +0300

    before bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,9 @@ class AlienInvasion():
 
         self.settings = Settings()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Вторгнення прибульців")
 
         self.ship = Ship(self)
@@ -27,16 +29,35 @@ class AlienInvasion():
 
 
     def _check_events(self):
-            # whatch for keyboard and mouse events.
+            """ whatch for keyboard and mouse events."""
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
                 elif event.type == pygame.KEYDOWN:
-                     if event.key == pygame.K_RIGHT:
-                          self.ship.moving_right = True
+                     self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                     if event.key == pygame.K_RIGHT:
-                          self.ship.moving_right = False
+                     self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event):
+         """Respond to keypresses"""
+         if event.key == pygame.K_RIGHT:
+             self.ship.moving_right = True
+         elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+         elif event.key == pygame.K_q:
+              sys.exit()
+        
+             
+
+    def _check_keyup_events(self, event):
+         """Respond to key releases"""
+         if event.key == pygame.K_RIGHT:
+             self.ship.moving_right = False
+         elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
                           
 
 
--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ class Settings():
     def __init__(self):
         """ Initialize the game`s settings"""
         # Screen settings
+        self.ship_speed = 2.5
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (23, 23, 23)
